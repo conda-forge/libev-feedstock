3c011062a9de0275b9d836e40be26bd2ce682d20
Mark Harfouche <mark.harfouche@gmail.com>
2023-12-07 18:49:38 -0500

    Try to add windows builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,12 @@
 # Get an updated config.sub and config.guess
+set -ex
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 ./configure --prefix="${PREFIX}"
 make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
 make check
+fi
+
+if [[ "${target_platform}" == "win-64" ]]; then
+    make install
 fi
